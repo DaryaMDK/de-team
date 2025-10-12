3b70525c494f4933bc9deafd6876388a349176f7
Nadya Kuznetsova <nskuznetsova0482@gmail.com>
2025-10-12 16:13:58 +0300

    Добавленние к hunter.py

--- hunter.py
+++ hunter.py
@@ -17,3 +17,28 @@ if a in hunters_power:
     print(f"Уровень силы {a}:", hunters_power[a])
 else:
     print("Такой охотник не найден в архиве.")
+
+hunters_power = {
+    "Джин": 85,
+    "Дон": 90,
+    "Сим": 88,
+    "Андрей": 70,
+    "Богги": 95,
+    "Кастиель": 95,
+    "Ребекка": 80,
+    "Люцифер": 100,
+    "Микаэль": 98,
+    "Габриэль": 92
+}
+
+
+def count_hunters():
+    print("Всего охотников в базе:", len(hunters_power))
+a = input()
+
+if a in hunters_power:
+    print(f"Уровень силы {a}:", hunters_power[a])
+else:
+    print("Такой охотник не найден в архиве.")
+count_hunters()
+
