676787e50cedc42db6617228ba1972b567cfabe9
bigpinklord <165569817+bigpinklord@users.noreply.github.com>
2025-10-12 14:40:01 +0300

    Add files via upload

---
+++ Matrix (example).py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+rows = int(input("Введите количество строк: "))
+
+cols = int(input("Введите количество столбцов: "))
+
+matrix_a = np.random.randint(0, 10, size=(rows, cols))
+
+matrix_b = np.random.randint(0, 10, size=(rows, cols))
+
+matrix_sum = matrix_a + matrix_b
+
+print("\nМатрица A:")
+
+print(matrix_a)
+
+print("\nМатрица B:")
+
+print(matrix_b)
+
+print("\nСумма матриц:")
+
+print(matrix_sum)
+
